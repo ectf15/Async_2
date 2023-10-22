e62476c868abe41a8fedf155a11c455487f263ed
ectf15 <ectineof@gmail.com>
2023-10-22 20:40:17 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Random random = new Random();
+
+while (true)
+{
+    string nombre;
+    Console.WriteLine("Ingrese un nombre: ");
+    nombre = Console.ReadLine();
+    DescargarInformacion(nombre);
+}
+
+async Task DescargarInformacion(string nombre){
+    
+    int tiempoAleatorio = random.Next(10000, 60000);
+    await Task.Delay(tiempoAleatorio);
+
+    Debug.WriteLine($"Información de {nombre} descargada con éxito");
+}
